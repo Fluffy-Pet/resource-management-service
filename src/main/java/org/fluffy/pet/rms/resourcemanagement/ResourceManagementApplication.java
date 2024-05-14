2ef467d78f4a0abca6e3b28d2c1ea4efcd0b5fa9
DevilsZone <akash@grayfrost.app>
2024-05-14 22:24:37 +0530

    Test Commit Rollback

--- src/main/java/org/fluffy/pet/rms/resourcemanagement/ResourceManagementApplication.java
+++ src/main/java/org/fluffy/pet/rms/resourcemanagement/ResourceManagementApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResourceManagementApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceManagementApplication.class, args);
 	}
