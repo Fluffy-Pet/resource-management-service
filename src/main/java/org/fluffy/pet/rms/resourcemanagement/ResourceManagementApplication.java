83c64b1ea22819a8ba27290a7ab79f2a0cd9302f
DevilsZone <akash@grayfrost.app>
2024-05-14 22:28:19 +0530

    New Test Commit

--- src/main/java/org/fluffy/pet/rms/resourcemanagement/ResourceManagementApplication.java
+++ src/main/java/org/fluffy/pet/rms/resourcemanagement/ResourceManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResourceManagementApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceManagementApplication.class, args);
 	}
